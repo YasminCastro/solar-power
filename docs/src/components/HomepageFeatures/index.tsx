6d1ceba057e07fed77614886a47238c65120b9d0
Yasmin <yasminsdcastro@gmail.com>
2023-04-10 20:25:55 -0300

    feat: home page

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,51 +1,71 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  img: string;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Yasmin",
+    img: require("@site/static/img/book.png").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <a
+          href="https://www.linkedin.com/in/yasmin-castro-b579451b8/"
+          target="_blank"
+        >
+          Linkedin
+        </a>
+        <br />
+        <a href="https://github.com/YasminCastro" target="_blank">
+          Github
+        </a>
+        <br />
+        <a href="mailto: yasminsdcastro@gmail.com" target="_blank">
+          yasminsdcastro@gmail.com
+        </a>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "IFG",
+    img: require("@site/static/img/light-bulb.png").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <a href="https://www.ifg.edu.br/">https://www.ifg.edu.br/</a>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Gabriel",
+    img: require("@site/static/img/brainstorm.png").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <a href="https://www.linkedin.com/in/gabrielmarqso/" target="_blank">
+          Linkedin
+        </a>
+        <br />
+        <a href="https://github.com/gabrielmarqso" target="_blank">
+          Github
+        </a>
+        <br />
+        <a href="mailto: gabrielmarquesmtv123@gmail.com" target="_blank">
+          gabrielmarquesmtv123@gmail.com
+        </a>
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, img, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={img} className={styles.featureImg} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
