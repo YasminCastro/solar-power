5a00500ed1c1f3a6ac39c04d3bb047bd45d3bb87
Yasmin <yasminsdcastro@gmail.com>
2023-05-21 19:40:31 -0300

    feat: fonts and colors

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  Ubuntu_700Bold,
+  Ubuntu_500Medium,
+  Ubuntu_400Regular,
+  useFonts,
+} from "@expo-google-fonts/ubuntu";
+
+import { Poppins_700Bold } from "@expo-google-fonts/poppins";
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Ubuntu_700Bold,
+    Ubuntu_500Medium,
+    Ubuntu_400Regular,
+    Poppins_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
