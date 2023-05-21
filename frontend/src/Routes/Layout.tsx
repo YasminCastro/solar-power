779cd376196037536080d78f500a8967f2ac51b4
gabrielmarqso <gabrielmarquesmtv123@gmail.com>
2023-05-21 20:49:45 -0300

    estruturação das pastas

---
+++ frontend/src/Routes/Layout.tsx
@@ -0,0 +1,48 @@
+import { View, Text, StyleSheet, Button } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useAuth } from "../context/AuthContext";
+import Home from "../screens/Home";
+import Login from "../screens/Login";
+import Profile from "../screens/Profile";
+import SignUp from "../screens/SignUp";
+
+const Stack = createNativeStackNavigator();
+
+const Layout = () => {
+  const { autState, onLogout } = useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {autState?.isAuth ? (
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerRight: () => <Button onPress={onLogout} title="Sair" />,
+            }}
+          />
+        ) : (
+          <Stack.Screen
+            name="Perfil"
+            component={SignUp}
+            options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Layout;
