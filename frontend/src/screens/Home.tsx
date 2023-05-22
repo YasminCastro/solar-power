3c636c366e764475613846915cfdf3e973a92a50
Yasmin <yasminsdcastro@gmail.com>
2023-05-22 18:43:19 -0300

    feat: home page

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,12 +1,57 @@
-import { Text, SafeAreaView } from "react-native";
-import React from "react";
+import { Text, SafeAreaView, View, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import { useAuth } from "../contexts/AuthContext";
+
+import api from "../lib/api";
+import Spinner from "react-native-loading-spinner-overlay/lib";
 
 const Home = () => {
+  const [powerGenerated, setPowerGenerated] = useState<any>(null);
+  const { user, authState } = useAuth();
+
+  async function loadPowerGenerated() {
+    const response = await api.get("/users/1", {
+      headers: {
+        Authorization: `Bearer ${authState.token}`,
+      },
+    });
+
+    setPowerGenerated(response.data.powerGenerated[0]);
+  }
+
+  useEffect(() => {
+    loadPowerGenerated();
+  }, []);
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-blueDark-500">
       <StatusBar style="auto" />
+      <View className="absolute top-24 flex-row justify-between">
+        <View className="flex-row">
+          <Text className="text-yellow-300">LOGO</Text>
+          <Text className="font-title text-2xl text-white">Bom dia!</Text>
+        </View>
+        <Pressable>
+          <Ionicons name="notifications" size={24} color="#FEBE3D" />
+        </Pressable>
+      </View>
+      {powerGenerated ? (
+        <View>
+          <Text className="text-number text-2xl text-white">
+            Tempo real: {powerGenerated.powerInRealTime}kW
+          </Text>
+          <Text className="text-number text-2xl text-white">
+            Produção Hoje: {powerGenerated.powerToday}KWh
+          </Text>
+          <Text className="text-number text-2xl text-white">
+            Tempo: {powerGenerated.tempC}° C
+          </Text>
+        </View>
+      ) : (
+        <Spinner visible={true} color="#FEBE3D" />
+      )}
     </SafeAreaView>
   );
 };
