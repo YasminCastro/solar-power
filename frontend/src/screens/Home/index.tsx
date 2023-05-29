6ca3ba767b8538b0e7010fa4408199866ff2f7b0
Yasmin <yasminsdcastro@gmail.com>
2023-05-29 16:52:48 -0300

    feat: refresh page

--- frontend/src/screens/Home/index.tsx
+++ frontend/src/screens/Home/index.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { ScrollView, RefreshControl } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
 import WelcomeView from "../../components/Screens/Home/WelcomeView";
-import { SafeAreaView } from "react-native-safe-area-context";
 import RealTimeView from "../../components/Screens/Home/RealTimeView";
 import TodayGraph from "../../components/Screens/Home/TodayGraph";
-import { ScrollView } from "react-native";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [key, setKey] = useState(0);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setKey((prevKey) => prevKey + 1);
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   return (
     <SafeAreaView className="flex-1 bg-blueDark-500 pt-1">
-      <ScrollView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
         <StatusBar style="light" />
         <WelcomeView />
-        <RealTimeView />
-        <TodayGraph />
+        <RealTimeView key={key + 1} />
+        <TodayGraph key={key + 2} />
       </ScrollView>
     </SafeAreaView>
   );
