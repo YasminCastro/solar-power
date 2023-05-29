63df2137c743a6357cdedf3d997790edbfcdf9b1
Yasmin <yasminsdcastro@gmail.com>
2023-05-29 17:36:00 -0300

    feat: todays graph

--- frontend/src/components/Screens/Home/TodayGraph.tsx
+++ frontend/src/components/Screens/Home/TodayGraph.tsx
@@ -1,58 +1,50 @@
-import { View, Text, Dimensions } from "react-native";
-import CircleChart from "./CircleChart";
+import { View, Dimensions, Text } from "react-native";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../../contexts/AuthContext";
 import { useUser } from "../../../contexts/UserContext";
 import PowerGenerated from "../../../interfaces/powerGenerated";
 import api from "../../../lib/api";
-import SemiCircleText from "../../Global/SemiCircleText";
-import { Entypo } from "@expo/vector-icons";
 
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-} from "react-native-chart-kit";
+import { LineChart } from "react-native-chart-kit";
+import moment from "moment";
 
 export default function TodayGraph() {
-  const [powerGenerated, setPowerGenerated] = useState<PowerGenerated | null>(
-    null
-  );
-  const [maxValue, setMaxValue] = useState<number>(100);
+  const [label, setLabel] = useState<string[]>([]);
+  const [dataset, setDataset] = useState<number[]>([]);
   const { authState } = useAuth();
   const { user } = useUser();
 
   async function loadPowerGenerated() {
     if (user && user?.inversors) {
       let invertersId: number[] = [];
-      let maxValuSum = 0;
 
       user?.inversors.forEach((inverter) => {
         if (inverter.active) {
-          maxValuSum += inverter.maxRealTimePower;
+          invertersId.push(inverter.id);
         }
       });
-      setMaxValue(maxValuSum);
-
-      let params: any = { userId: user.id };
-      if (invertersId.length === 1) {
-        params.inverterId = invertersId[0].toString();
-        params.limit = 1;
-      } else if (invertersId.length > 1) {
-        params.invertersId = invertersId.toString();
-      }
 
       const { data } = await api.get(`/power-generated`, {
-        params,
+        params: {
+          userId: user.id,
+          inverterId: invertersId[0],
+          today: true,
+        },
         headers: {
           Authorization: `Bearer ${authState.token}`,
         },
       });
 
-      setPowerGenerated(data[0]);
+      setDataset([]);
+      setLabel([]);
+
+      data.forEach((element: PowerGenerated) => {
+        const parsedDate = moment(element.createdAt).format("HH");
+        const parseData = parseFloat(element.powerInRealTime);
+        setLabel((prev) => [...prev, parsedDate]);
+
+        setDataset((prev) => [...prev, parseData]);
+      });
     }
   }
 
@@ -62,38 +54,46 @@ export default function TodayGraph() {
 
   return (
     <View className="mt-4 items-center">
-      <LineChart
-        data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
-          datasets: [
-            {
-              data: [1, 2, 3, 4, 5, 6],
-            },
-          ],
-        }}
-        width={Dimensions.get("window").width - 40}
-        height={280}
-        yAxisInterval={1}
-        onDataPointClick={(data) => console.log(data)}
-        chartConfig={{
-          backgroundColor: "#7179A5",
-          backgroundGradientFrom: `rgba(113, 121, 165, 100)`,
-          backgroundGradientTo: `rgba(111, 119, 195, 0)`,
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-          propsForDots: {
-            fill: "#FEA259",
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
+      {label.length > 0 && dataset.length > 0 && (
+        <>
+          <LineChart
+            data={{
+              labels: label,
+              datasets: [
+                {
+                  data: dataset,
+                },
+              ],
+            }}
+            width={Dimensions.get("window").width - 40}
+            height={280}
+            yAxisInterval={1}
+            formatYLabel={(yLabel) => `${yLabel}kW`}
+            onDataPointClick={(data) => console.log(data)}
+            chartConfig={{
+              backgroundColor: "#7179A5",
+              backgroundGradientFrom: `rgba(113, 121, 165, 100)`,
+              backgroundGradientTo: `rgba(111, 119, 195, 0)`,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                fill: "#FEA259",
+              },
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+          />
+          <Text className="font-body text-sm text-gray-50">
+            Rendimento hoje
+          </Text>
+        </>
+      )}
     </View>
   );
 }
