5dc5e9a962f38d74afd4c80bfed21d72c2072812
Yasmin Castro <yasminsdcastro@gmail.com>
2023-06-19 18:33:30 -0300

    remove: prisma

--- backend/src/test/index.test.ts
+++ backend/src/test/index.test.ts
@@ -1,6 +1,6 @@
+import { App } from '@/app';
+import { AuthRoute } from '@/routes/auth.route';
 import request from 'supertest';
-import App from '@/app';
-import IndexRoute from '@routes/index.route';
 
 afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
@@ -9,7 +9,7 @@ afterAll(async () => {
 describe('Testing Index', () => {
   describe('[GET] /', () => {
     it('response statusCode 200', () => {
-      const indexRoute = new IndexRoute();
+      const indexRoute = new AuthRoute();
       const app = new App([indexRoute]);
 
       return request(app.getServer()).get(`${indexRoute.path}`).expect(200);
