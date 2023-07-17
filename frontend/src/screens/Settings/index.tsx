a0d3275ad3e3af4fa785f0a14c4be0ed4a7486fe
Yasmin <yasminsdcastro@gmail.com>
2023-07-17 14:27:10 -0300

    feat: settings

--- frontend/src/screens/Settings/index.tsx
+++ frontend/src/screens/Settings/index.tsx
@@ -5,17 +5,18 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const Settings: React.FC = () => {
   const { signOut } = useAuth();
 
-  function handleSingOut() {
-    signOut();
-  }
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-blueDark-500">
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text className="font-body text-base text-gray-200">Sair</Text>
+      </TouchableOpacity>
+      <View></View>
       <TouchableOpacity
         className="flex h-12 flex-row items-center justify-center rounded-full bg-solar-100 px-6"
-        onPress={handleSingOut}
+        onPress={() => console.log("INVERTER")}
       >
         <View className="flex flex-1 items-center">
-          <Text className="font-body text-base text-solar-500">Sair</Text>
+          <Text className="font-body text-base text-solar-500">Inversor</Text>
         </View>
       </TouchableOpacity>
     </SafeAreaView>
