8e9cd6b7b47306042143890da39947bb2bc9604f
Yasmin Castro <yasminsdcastro@gmail.com>
2023-08-23 15:49:04 -0300

    refactor: url prod

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,7 +1,10 @@
 import { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } from '@config';
 
+const prodUrl = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`;
+const devUrl = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
 export const dbConnection = {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`,
+  url: prodUrl,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
