36a26bd97d28a307105a15985c79563fa2e957bc
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-12 20:20:27 -0300

    feat: get user test

---
+++ backend/src/test/users.test.ts
@@ -0,0 +1,58 @@
+import { App } from '@/app';
+import { UserModel } from '@/models/users.models';
+import { UserRoute } from '@/routes/users.route';
+import { AuthRoute } from '@/routes/auth.route';
+import request from 'supertest';
+
+describe('Auth Router', () => {
+  let app: App;
+  let usersRoute: UserRoute;
+  let authRoute: AuthRoute;
+  const email = 'test@example.com';
+  const password = '123456789';
+  let token: string;
+  let userId: string;
+
+  beforeAll(async () => {
+    usersRoute = new UserRoute();
+    authRoute = new AuthRoute();
+    app = new App([usersRoute, authRoute]);
+
+    // Criar um usuário para teste
+    const userData = {
+      name: 'Test User',
+      email,
+      password,
+    };
+
+    await UserModel.deleteOne({ email });
+
+    const response = await request(app.getServer()).post('/signup').send(userData);
+    token = response.body.token;
+    userId = response.body.user._id;
+  });
+
+  afterAll(async () => {
+    await app.closeDatabaseConnection();
+    await app.closeQueueConnections();
+  });
+
+  describe('[GET] /users', () => {
+    it('response should return status 200 and an array of users', async () => {
+      const response = await request(app.getServer()).get('/users');
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body)).toBeTruthy();
+    });
+  });
+
+  describe('[GET] /users/:id', () => {
+    it('response should return status 200 and the user data', async () => {
+      const response = await request(app.getServer()).get(`/users/${userId}`).set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toBeDefined();
+      expect(response.body._id).toBe(userId);
+    });
+  });
+});
