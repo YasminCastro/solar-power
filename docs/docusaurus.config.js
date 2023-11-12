a4a249528d8bfa00409bc4e1190817ccac706481
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-12 12:33:01 -0300

    feat: open api

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,38 +9,27 @@ const config = {
   title: "Solar Power",
   tagline:
     "Um aplicativo móvel  para incentivar a otimização e  economia de energia em sistemas  de energia solar.",
-
-  favicon: "img/logo.svg",
-
-  // Set the production url of your site here
   url: "https://your-docusaurus-test-site.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "ifg", // Usually your GitHub org/user name.
-  projectName: "solar-power", // Usually your repo name.
-
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  favicon: "img/logo.svg",
+  organizationName: "ifg",
+  projectName: "solar-power",
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "pt-BR",
+    locales: ["pt-BR"],
   },
-
   presets: [
     [
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/PaloAltoNetworks/docusaurus-openapi-docs/tree/main/demo",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -52,8 +41,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       navbar: {
         title: "Solar Power",
         logo: {
@@ -62,10 +54,25 @@ const config = {
         },
         items: [
           {
-            type: "docSidebar",
-            sidebarId: "docsSidebar",
+            type: "doc",
+            docId: "intro",
             position: "left",
             label: "Docs",
+          },
+          {
+            type: "dropdown",
+            label: "API",
+            position: "left",
+            items: [
+              {
+                label: "API Zoo",
+                to: "/category/petstore-api",
+              },
+              {
+                label: "Petstore (versioned)",
+                to: "/category/petstore-versioned-api",
+              },
+            ],
           },
           {
             position: "left",
@@ -105,12 +112,71 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Solar Power, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © 2023 Solar Power, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      plugins: [
+        [
+          "docusaurus-plugin-openapi-docs",
+          {
+            id: "openapi",
+            docsPluginId: "classic",
+            config: {
+              petstore_versioned: {
+                specPath: "/examples/petstore.yaml",
+                outputDir: "docs/petstore_versioned", // No trailing slash
+                sidebarOptions: {
+                  groupPathsBy: "tag",
+                  categoryLinkSource: "tag",
+                },
+                version: "2.0.0", // Current version
+                label: "v2.0.0", // Current version label
+                baseUrl: "/petstore_versioned/swagger-petstore-yaml", // Leading slash is important
+                versions: {
+                  "1.0.0": {
+                    specPath: "examples/petstore-1.0.0.yaml",
+                    outputDir: "docs/petstore_versioned/1.0.0", // No trailing slash
+                    label: "v1.0.0",
+                    baseUrl: "/petstore_versioned/1.0.0/swagger-petstore-yaml", // Leading slash is important
+                  },
+                },
+              },
+              petstore: {
+                specPath: "examples/petstore.yaml",
+                proxy: "https://cors.pan.dev",
+                outputDir: "docs/petstore",
+                sidebarOptions: {
+                  groupPathsBy: "tag",
+                  categoryLinkSource: "tag",
+                },
+                template: "api.mustache", // Customize API MDX with mustache template
+                downloadUrl:
+                  "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-openapi-docs/main/demo/examples/petstore.yaml",
+                hideSendButton: false,
+              },
+              cos: {
+                specPath: "examples/openapi-cos.json",
+                outputDir: "docs/cos",
+                sidebarOptions: {
+                  groupPathsBy: "tag",
+                },
+              },
+              burgers: {
+                specPath: "examples/food/burgers/openapi.yaml",
+                outputDir: "docs/food/burgers",
+              },
+              yogurt: {
+                specPath: "examples/food/yogurtstore/openapi.yaml",
+                outputDir: "docs/food/yogurtstore",
+              },
+            },
+          },
+        ],
+      ],
+      themes: ["docusaurus-theme-openapi-docs"],
     }),
 };
 
