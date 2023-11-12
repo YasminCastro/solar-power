197180275ffe58e630f22c74d11518b93583c7e8
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-12 20:46:01 -0300

    feat; test get inverters

--- backend/src/test/inverters.test.ts
+++ backend/src/test/inverters.test.ts
@@ -60,39 +60,12 @@ describe('Inverters Router', () => {
     });
   });
 
-  // describe('[GET] /users/:id', () => {
-  //   it('response should return status 200 and the user data', async () => {
-  //     const response = await request(app.getServer()).get(`/users/${userId}`).set('Authorization', `Bearer ${token}`);
+  describe('[GET] /inverters', () => {
+    it('response should return status 200 and an array of inverters', async () => {
+      const response = await request(app.getServer()).get('/inverters').set('Authorization', `Bearer ${token}`);
 
-  //     expect(response.status).toBe(200);
-  //     expect(response.body).toBeDefined();
-  //     expect(response.body._id).toBe(userId);
-  //   });
-  // });
-
-  // describe('[PUT] /users/:id', () => {
-  //   it('response should return status 200 and the updated user data', async () => {
-  //     const updatedUserData = {
-  //       name: 'Updated Name',
-  //     };
-
-  //     const response = await request(app.getServer()).put(`/users/${userId}`).send(updatedUserData).set('Authorization', `Bearer ${token}`);
-
-  //     expect(response.status).toBe(200);
-  //     expect(response.body).toBeDefined();
-  //     expect(response.body.user._id).toBe(userId);
-  //     expect(response.body.user.name).toBe(updatedUserData.name);
-  //     expect(response.body.message).toBe('User successfully updated');
-  //   });
-  // });
-
-  // describe('[DELETE] /users/:id', () => {
-  //   it('response should return status 200 and the success message', async () => {
-  //     const response = await request(app.getServer()).delete(`/users/${userId}`).set('Authorization', `Bearer ${token}`);
-
-  //     expect(response.status).toBe(200);
-  //     expect(response.body).toBeDefined();
-  //     expect(response.body.message).toBe('User successfully deleted');
-  //   });
-  // });
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body)).toBeTruthy();
+    });
+  });
 });
