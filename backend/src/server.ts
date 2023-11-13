e2da6862e0965862b06a852a14ffb0b589f08cb7
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-12 23:55:37 -0300

    remove: utils stuff

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,6 @@ import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
 import { InvertersRoute } from './routes/inverters.route';
 import { PowerGeneratedRoute } from './routes/powerGenerated.route';
-import { UtilsRoute } from './routes/utils.route';
 import { SolarDataRoute } from './routes/solarData';
 import { AchivementsRoute } from './routes/achivements.route';
 
@@ -15,7 +14,6 @@ const app = new App([
   new AuthRoute(),
   new InvertersRoute(),
   new PowerGeneratedRoute(),
-  new UtilsRoute(),
   new SolarDataRoute(),
   new AchivementsRoute(),
 ]);
