fd0218e2983e0235ad69879e2634a638aedfb26d
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-13 15:47:40 -0300

    refactor: aumentar tempo cron

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -102,7 +102,7 @@ export class App {
 
   private initializeCronJob() {
     let job = new CronJob(
-      '0 */5 6-18 * * *',
+      '*/10 5-19 * * *',
       async function () {
         logger.info('Runing cronjob:' + moment().format('DD-MM-YYYY HH:mm:ss'));
 
