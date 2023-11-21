f8baef9078b54f23678ed09b35d58c2b90e8f080
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-21 11:18:57 -0300

    feat: types

--- frontend/src/utils/autoKWhConvert.ts
+++ frontend/src/utils/autoKWhConvert.ts
@@ -1,6 +1,11 @@
-export function autoKWhConvert(value: number) {
+interface IReturn {
+  suffix: string;
+  value: number;
+}
+
+export function autoKWhConvert(value: number): IReturn {
   if (value >= 1000) {
-    return { suffix: "MWh", value: (value / 1000).toFixed(2) };
+    return { suffix: "MWh", value: parseFloat((value / 1000).toFixed(2)) };
   }
 
   return { suffix: "kWh", value: value };
